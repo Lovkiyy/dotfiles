eb857646d2b55ce55937847feb186a509b16f76d
Lovkiy <leonidb2009@gmail.com>
2019-04-06 20:39:11 +0300

    minor changes

--- .scripts/vtype.sh
+++ .scripts/vtype.sh
@@ -6,7 +6,7 @@ xsel -b -o > $file
 # For some reason i can't fold file in place
 change=$(fold -w 80 -s $file)
 echo -e "$change" > $file
-sed -i 's/^[ \t]*//;s/[ \t]*$//g' "$file"
+sed -i "s/’/\'/;s/–/-/;s/–/-/;s/’/\'/;s/’/\'/;s/‘/\'/;s/”/\"/;s/“/\"/;s/‘/\'/;s/’/\'/;s/–/-/;s/^[ \t]*//;s/[ \t]*$//g" "$file"
 
 nvim -d $file " "
 rm -f $file
