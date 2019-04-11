c0c09664d90b67515c5023cb8f25051458ae0dc3
Lovkiy <leonidb2009@gmail.com>
2019-04-11 13:35:20 +0300

    change paths for scripts

--- .scripts/fzf-notes.sh
+++ .scripts/fzf-notes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-choice="$(find -L $HOME/notes -type f -not \( -path '*/.git/*' -o -path '*.jpg' -o -path '*.png' \) | fzf --height 8)"
+choice="$(find -L $HOME/usr/notes -type f -not \( -path '*/.git/*' -o -path '*.jpg' -o -path '*.png' \) | fzf --height 8)"
 if [ $choice ]; then
   $EDITOR $choice
 fi
