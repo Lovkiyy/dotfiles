21b5e95a67cd2ac352cb464af9d9396ce863c54c
Lovkiy <leonidb2009@gmail.com>
2019-05-01 22:27:39 +0300

    add noequalalways option to nvim

--- .scripts/vtype.sh
+++ .scripts/vtype.sh
@@ -4,9 +4,9 @@ file=$(mktemp /tmp/vtype.XXXXX)
 xsel -b -o > $file
 
 # For some reason i can't fold file in place
-change=$(fold -w 80 -s $file)
-echo -e "$change" > $file
-sed -i "s/’/\'/;s/–/-/;s/–/-/;s/’/\'/;s/’/\'/;s/‘/\'/;s/”/\"/;s/“/\"/;s/‘/\'/;s/’/\'/;s/–/-/;s/^[ \t]*//;s/[ \t]*$//g" "$file"
+change=$(fold -w 130 -s $file)
+echo "$change" > $file
+sed -i "s/—/-/;s/—/-/;s/…/.../;s/’/\'/;s/–/-/;s/–/-/;s/’/\'/;s/’/\'/;s/‘/\'/;s/”/\"/;s/“/\"/;s/‘/\'/;s/’/\'/;s/–/-/;s/^[ \t]*//;s/[ \t]*$//g" "$file"
 
-nvim -d $file " "
+nvim -c ":set noequalalways" -d $file " "
 rm -f $file
