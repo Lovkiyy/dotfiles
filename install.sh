daf4806218841e558329575af4ff0e6c1fbe98ce
Lovkiy <leonidb2009@gmail.com>
2019-06-18 09:42:07 +0300

    replace dotinstall.sh script with new one

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# TODO: check on files/directories with spaces, check whether it works with ending slash in $src
+src="$HOME/src/dotfiles/.config"
+dest="${XDG_CONFIG_HOME:-$HOME/.config}"
+ln_opts="-s -i"
+script_name="$(basename $0)"
+
+if [ ! -d $src ];then
+  echo $script_name: $src does not exist or is not a directory; exit 1
+fi
+
+if [ ! -d $dest ];then
+  echo $script_name: $dest does not exist or is not a directory; exit 1
+fi
+
+if [ ! -w $dest ];then
+  echo $script_name: $dest is not writable; exit 1
+fi
+
+dir="$src/*/*"
+for i in $dir; do
+  destfile="${dest}${i#$src}"
+  if [ ! -e $(dirname $destfile) ];then
+    mkdir $(dirname $destfile)
+  fi
+  ln $ln_opts $i $destfile
+done
