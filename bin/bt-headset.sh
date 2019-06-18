4e36839caeaf2ab8bc5719d0db45b86bb41aecc8
Lovkiy <leonidb2009@gmail.com>
2019-06-18 08:47:26 +0300

    make mac address a variable in bt-headset.sh

--- bin/bt-headset.sh
+++ bin/bt-headset.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+MAC_ADDR="78:44:05:B0:29:9F"
 bluetoothctl power on
 if [ -z "$(pgrep pulseaudio)" ]; then
   pulseaudio -D
 fi
-bluetoothctl connect 78:44:05:B0:29:9F
+bluetoothctl connect $MAC_ADDR
