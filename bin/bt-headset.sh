77f18f07da9898c5610a2eb187885b61a0a4d36f
sleich <sleich@tutanota.com>
2019-08-06 17:46:25 +0900

    delete unneeded scripts

--- bin/bt-headset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-MAC_ADDR="78:44:05:B0:29:9F"
-bluetoothctl power on
-if [ -z "$(pgrep pulseaudio)" ]; then
-  pulseaudio -D
-fi
-bluetoothctl connect $MAC_ADDR
--- bin/colors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-C=0
-while [ $C -lt 255 ]; do
-    tput setab $C
-    echo -n "$C "
-    C=$((C + 1))
-done
-tput sgr0
-echo
