a30db7b6e040ab307107f11e9cf86b8c3df43d67
sleich <sleich@tutanota.com>
2019-09-14 10:48:37 +0300

    add script for getting mpris player status

---
+++ bin/mpris-status.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+PLAYER=cmus
+info="$(playerctl -p "$PLAYER" metadata --format '{{artist}} - {{title}}' 2>/dev/null)"
+status="$(playerctl -p "$PLAYER" status 2>/dev/null)"
+if [ "$status" = "Playing" ];then
+  echo "$info"
+elif [ "$status" = "Paused" ];then
+  echo "$info "
+fi
