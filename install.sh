7ab6a59e5e51808833a95a047b330c63b3031854
sleich <sleich@tutanota.com>
2019-10-04 15:21:24 +0300

    update install.sh to use function for symlinking things

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script doesn't handle filenames with spaces well
-src=$(realpath "$(dirname $0)")/.config
-dest="${XDG_CONFIG_HOME:-$HOME/.config}"
+src_config=$(realpath "$(dirname $0)")/.config
+dest_config="${XDG_CONFIG_HOME:-$HOME/.config}"
 ln_opts="-s -f -v"
 script_name="$(basename $0)"
 
@@ -17,7 +17,12 @@ if [ ! -w $dest ];then
   echo $script_name: $dest is not writable; exit 1
 fi
 
-cd "$src"
-mkdir -pv $(find -type d | sed "s|^\./|$dest/|")
-find -type f | sed 's|^\./||' | xargs -I _ ln $ln_opts "$src"/_ "$dest"/_
-cd "$OLDPWD"
+# Create symlinks to all the files in $1 in $2 recursively($1 and $2 are directories)
+# Automatically creates necessary subdirectories
+linkfiles () {
+  cd "$1"
+  mkdir -pv $(find -type d | sed "s|^\./|$2/|")
+  find -type f | sed 's|^\./||' | xargs -I _ ln $ln_opts "$1"/_ "$2"/_
+}
+
+linkfiles "$src_config" "$dest_config"
