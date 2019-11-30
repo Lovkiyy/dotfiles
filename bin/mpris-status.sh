8780af2915ead67dc11b617ff062756df7c81a62
sleich <sleich@tutanota.com>
2019-11-30 20:50:11 +0300

    output icon in mpris-status

--- bin/mpris-status.sh
+++ bin/mpris-status.sh
@@ -3,9 +3,9 @@ PLAYER=cmus
 info="$(playerctl -p "$PLAYER" metadata --format '{{artist}} - {{title}}' 2>/dev/null)"
 status="$(playerctl -p "$PLAYER" status 2>/dev/null)"
 if [ "$status" = "Playing" ];then
-  echo "$info"
+  echo " $info"
 elif [ "$status" = "Paused" ];then
-  echo "$info "
+  echo " $info "
 else
   echo ""
 fi
