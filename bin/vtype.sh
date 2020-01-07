ce7b2b15b839203212b71b95a6a44a381352ea9d
sleich <sleich@tutanota.com>
2020-01-07 19:26:29 +0300

    make some scripts work with Wayland while retaining support for Xorg

--- bin/vtype.sh
+++ bin/vtype.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+if [ "$WAYLAND_DISPLAY" ];then
+  PASTE_COMMAND="wl-paste"
+else
+  PASTE_COMMAND="xclip -o -selection clipboard"
+fi
+
 file=$(mktemp /tmp/vtype.XXXXX)
 
-xclip -o -selection clipboard > $file
+$PASTE_COMMAND > $file
 
 # For some reason i can't fold file in place
 change=$(fold -w 72 -s $file)
